4d0e39e392c7a3071025cca659bba1e49cd4e9e9
waynezhang <linghua.zhang@me.com>
2017-10-29 16:17:42 +0900

    mod. slightly fix

--- index.js
+++ index.js
@@ -2,35 +2,48 @@ var fs = require("fs");
 var Handlebars = require("handlebars");
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
-	// var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+  var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+  // var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-	// Nicer dates
-	Handlebars.registerHelper('date', function(date) {
-	  var theDate = new Date(date);
+  // Nicer dates
+  Handlebars.registerHelper('date', function(date) {
+    var theDate = new Date(date);
 
-	  return months[theDate.getMonth()] + ' ' + theDate.getFullYear();
-	});
+    return months[theDate.getMonth()] + ' ' + theDate.getFullYear();
+  });
 
-	return Handlebars.compile(template)({
-		css: css,
-		resume: resume
-	});
+  return Handlebars.compile(template)({
+    css: css,
+    resume: resume
+  });
 }
 
 Handlebars.registerHelper('paragraphSplit', function(plaintext) {
-    var i, output = '',
-        lines = plaintext.split(/\r\n|\r|\n/g);
-    for (i = 0; i < lines.length; i++) {
-        if(lines[i]) {
-            output += '<p>' + lines[i] + '</p>';
-        }
+  var i, output = '',
+    lines = plaintext.split(/\r\n|\r|\n/g);
+  for (i = 0; i < lines.length; i++) {
+    if(lines[i]) {
+      output += '<p>' + lines[i] + '</p>';
     }
-    return new Handlebars.SafeString(output);
+  }
+  return new Handlebars.SafeString(output);
+});
+
+Handlebars.registerHelper('listSplit', function(plaintext) {
+  var i, output = '',
+    lines = plaintext.split(/\r\n|\r|\n/g);
+  output += "<ul class='list'>";
+  for (i = 0; i < lines.length; i++) {
+    if(lines[i]) {
+      output += '<li>' + lines[i] + '</li>';
+    }
+  }
+  output += "</ul>";
+  return new Handlebars.SafeString(output);
 });
 
 module.exports = {
-	render: render
+  render: render
 };
